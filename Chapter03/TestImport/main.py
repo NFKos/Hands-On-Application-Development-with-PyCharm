9ffc4e7e9bd4d2f3e097cd82c45a574ba8e8060a
NFKos <111289891+NFKos@users.noreply.github.com>
2022-08-16 17:07:54 +0200

    Delete Chapter03 directory
    Just for testing purpuses because I'm not able to clone the repository in my PyCharm because of this chapter

--- Chapter03/TestImport/main.py
+++
@@ -1,8 +0,0 @@
-if __name__ == '__main__':
-    print('You have successfully imported this project.')
-
-    try:
-        import tqdm
-        print('You also have tqdm!')
-    except ModuleNotFoundError:
-        print('You do not have tqdm.')
